698e1e9c5f2351e2506b393f85ac5dfc2eb23270
SEBAS0o0 <92197217+SEBAS0o0@users.noreply.github.com>
2023-02-01 07:15:56 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const container = document.getElementById("container");
+
+const hideContainer = () => {
+  container.classList.add("close");
+  container.style.display = "none";
+};
+
+setTimeout(hideContainer, 3000);
+
+
+
+
+
+
